77ba2c3ef4394e6067b168328bdeb620f121910d
Oscar S Kolsrud <oscar.sortland.kolsrud@gmail.com>
2019-02-06 08:56:09 +0100

    Update LCD_Test.ino

--- LCD_Test/LCD_Test.ino
+++ LCD_Test/LCD_Test.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+// Include needed libraries
+#include <LiquidCrystal.h>
 
+// Initialize the LCD library
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // initialize the serial communications:
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // when characters arrive over the serial port...
+  if (Serial.available()) {
+    // wait a bit for the entire message to arrive
+    delay(100);
+    // clear the screen
+    lcd.clear();
+    // read all the available characters
+    while (Serial.available() > 0) {
+      // display each character to the LCD
+      lcd.write(Serial.read());
+    }
+  }
 }
